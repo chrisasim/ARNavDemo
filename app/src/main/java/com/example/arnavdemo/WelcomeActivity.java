96c24aac9d706d7bc8f631e009d56d9f091bcf4c
Spyros <spuros.tsotzo@gmail.com>
2021-04-06 04:29:13 +0300

    initial commit

--- app/src/main/java/com/example/arnavdemo/WelcomeActivity.java
+++ app/src/main/java/com/example/arnavdemo/WelcomeActivity.java
@@ -8,16 +8,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private Button continueButton;
+    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        continueButton = findViewById(R.id.continue_button);
+        btnContinue = findViewById(R.id.continue_button);
 
-        continueButton.setOnClickListener(new View.OnClickListener() {
+        btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(WelcomeActivity.this, ExplanationApp.class);
