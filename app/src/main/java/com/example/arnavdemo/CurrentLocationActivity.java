3db2edc06d54e07715c4686fa1326704a52e25aa
Christodoulos Asiminidis <43860123+chrisasim@users.noreply.github.com>
2021-07-06 14:43:00 +0300

    Update CurrentLocationActivity.java
    Barcode and qr enhancements.

--- app/src/main/java/com/example/arnavdemo/CurrentLocationActivity.java
+++ app/src/main/java/com/example/arnavdemo/CurrentLocationActivity.java
@@ -60,7 +60,12 @@ public class CurrentLocationActivity extends AppCompatActivity {
         btnScanBarcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(CurrentLocationActivity.this, ScannedBarcodeActivity.class));
+                IntentIntegrator integrator = new IntentIntegrator(CurrentLocationActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan a barcode or QR code");
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
             }
         });
 
@@ -109,6 +114,31 @@ public class CurrentLocationActivity extends AppCompatActivity {
             }
         });
     }
-
-
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result  = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result!=null)
+        {
+            if(result.getContents()==null)
+            {
+                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                //
+                Intent intent = new Intent(CurrentLocationActivity.this, DestinationActivity.class);
+                intent.putIntegerArrayListExtra("coords", coordinates);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "You set your location successfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), String.valueOf(result.getFormatName()), Toast.LENGTH_SHORT);
+                Toast.makeText(getApplicationContext(), String.valueOf(result.getContents()), Toast.LENGTH_SHORT);
+            }
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
