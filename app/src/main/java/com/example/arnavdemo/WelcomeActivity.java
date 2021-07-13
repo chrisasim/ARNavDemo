0da17d9bba7694fe49ad6754c2940d519271414f
Christodoulos Asiminidis <43860123+chrisasim@users.noreply.github.com>
2021-07-13 15:01:02 +0300

    Update WelcomeActivity.java

--- app/src/main/java/com/example/arnavdemo/WelcomeActivity.java
+++ app/src/main/java/com/example/arnavdemo/WelcomeActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+       private String[] permission = {Manifest.permission.ACCESS_CHECKIN_PROPERTIES, Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        getUserPermission();
 
         Button btnContinue = findViewById(R.id.continue_button);
 
@@ -23,7 +25,27 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+       private void getUserPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(permission, 0);
+            }
+        }
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK)
+        {
+            Toast.makeText(WelcomeActivity.this, "Bluetooth is ON", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(WelcomeActivity.this, "Bluetooth operation is cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
