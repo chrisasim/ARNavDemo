567eedd10dd26089ef2a9f0fde1992810121f27a
Christodoulos Asiminidis <43860123+chrisasim@users.noreply.github.com>
2021-07-13 15:29:18 +0300

    Update WelcomeActivity.java

--- app/src/main/java/com/example/arnavdemo/WelcomeActivity.java
+++ app/src/main/java/com/example/arnavdemo/WelcomeActivity.java
@@ -8,12 +8,32 @@ import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+      public static final int BLUETOOTH_RED_CODE = 1;
+    private String[] permission = {Manifest.permission.ACCESS_CHECKIN_PROPERTIES, Manifest.permission.ACCESS_FINE_LOCATION};
+
+
     private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(getApplicationContext(), "Device does not support Bluetooth", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            if (!bluetoothAdapter.isEnabled())
+            {
+                startActivityForResult(new Intent(bluetoothAdapter.ACTION_REQUEST_ENABLE), BLUETOOTH_RED_CODE);
+                //Toast.makeText(getApplicationContext(), "Bluetooth turned on", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+        getUserPermission();
 
         btnContinue = findViewById(R.id.continue_button);
 
@@ -25,6 +45,15 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         });
 
+        private void getUserPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(permission, 0);
+            }
+        }
+    }
 
     }
 }
